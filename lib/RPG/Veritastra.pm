68c30c6822125d281e5bbc3e816b4c5018e982de
Iain C Docherty <github@iandocherty.com>
2013-09-24 12:07:19 -0400

    Added a few more files and some documentation.

---
+++ lib/RPG/Veritastra.pm
@@ -0,0 +1,51 @@
+package RPG::Veritastra;
+
+use strict;
+use Module::Find qw(useall);
+use Lacuna::DB;
+use Config::JSON;
+
+useall __PACKAGE__;
+
+our $VERSION = 0.0001;
+
+my $config = Config::JSON->new('/data/Veritastra/etc/veritastra.conf');
+my $db = RPG::Veritastra::DB->connect(
+    $config->get('db/dsn'),
+    $config->get('db/username'),
+    $config->get('db/password'), 
+    { mysql_enable_utf8 => 1},
+);
+
+my $cache = RPG::Veritastra::Cache->new(servers => $config->get('memcached'));
+my $queue;
+
+if ($config->get('beanstalk')) {
+    $queue = RPG::Veritastra::Queue->new({
+        server      => $config->get('beanstalk/server'),
+        ttr         => $config->get('beanstalk/ttr'),
+        debug       => $config->get('beanstalk/debug'),
+    });
+}
+
+sub version {
+    return $VERSION;
+}
+
+sub config {
+    return $config;
+}
+
+sub db {
+    return $db;
+}
+
+sub cache {
+    return $cache;
+}
+
+sub queue {
+    return $queue;
+}
+
+1;
